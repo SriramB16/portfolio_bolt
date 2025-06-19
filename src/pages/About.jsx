3dbbbd9ed0c21c817c60423ca5e1886883ca7247
Sriram <136231040+SriramB16@users.noreply.github.com>
2025-06-19 22:12:47 +0530

    Updated About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -171,7 +171,7 @@ const About = () => {
             <div className="lg:col-span-2 text-center lg:text-left lg:pl-8 xl:pl-12">
               <ScrollReveal direction="right" delay={0.2}>
                 <h1 className="font-clash text-3xl sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold leading-tight mb-6 sm:mb-8">
-                  A <span className="text-green-500">creative</span><br />
+                  A <span className="text-green-500">creative</span>
                   <span className="text-green-500">developer</span> &<br />
                   digital designer
                 </h1>
